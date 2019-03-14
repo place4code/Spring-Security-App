4c6c89689b55e485606e861dc004b5885f2bfe9e
place4code <place4code@gmail.com>
2019-03-14 16:14:56 +0100

    add jdbc auth

--- src/main/java/com/place4code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/place4code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.place4code.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +11,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		//add users
-		@SuppressWarnings("deprecation")
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("Luc").password("pass").roles("ADMIN", "MANAGER", "USER"))
-		.withUser(users.username("Mark").password("pass").roles("MANAGER", "USER"))
-		.withUser(users.username("Oli").password("pass").roles("USER"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
 	}
 
@@ -30,7 +30,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-		.antMatchers("/").hasRole("USER")
+		.antMatchers("/").hasRole("EMPLOYEE")
 		.antMatchers("/leaders/**").hasRole("MANAGER")
 		.antMatchers("/systems/**").hasRole("ADMIN")
 		.antMatchers("/resources/**").permitAll()
