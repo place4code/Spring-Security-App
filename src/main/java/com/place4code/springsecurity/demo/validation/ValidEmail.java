4957add02e983330567a2f5d3cfb7fe0e762b41f
place4code <place4code@gmail.com>
2019-04-05 11:27:45 +0200

    add fieldValidator

--- src/main/java/com/place4code/springsecurity/demo/validation/ValidEmail.java
+++ src/main/java/com/place4code/springsecurity/demo/validation/ValidEmail.java
@@ -10,15 +10,13 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Constraint(validatedBy = EmailValidator.class)
-@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-
 public @interface ValidEmail {
-
 	String message() default "Invalid email";
-	
+
 	Class<?>[] groups() default {};
-	
+
 	Class<? extends Payload>[] payload() default {};
 }
